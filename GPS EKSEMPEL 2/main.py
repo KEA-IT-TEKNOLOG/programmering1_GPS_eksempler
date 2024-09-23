65f84eeea6fcc9f4b0e7cc94531833942b2b89b0
KevinLindemark <58036568+KevinLindemark@users.noreply.github.com>
2024-09-23 15:21:56 +0200

    fixed comments and removed unused code

--- GPS EKSEMPEL 2/main.py
+++ GPS EKSEMPEL 2/main.py
@@ -15,16 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-"""
-links and resources used:
-https://thingsboard.io/docs/user-guide/rpc/
-https://github.com/manuargue/thingsboard-micropython
-https://www.youtube.com/watch?v=EWJAZA_44ro
-
-"""
 from uthingsboard.client import TBDeviceMqttClient
 from time import sleep
-from random import randint, uniform
 from machine import reset
 import gc
 import secrets
@@ -37,52 +29,48 @@ gps_speed = 9600                           # UART speed, defauls u-blox speed
 #########################################################################
 # OBJECTS
 uart = UART(gps_port, gps_speed)           # UART object creation
-gps = GPS_SIMPLE(uart)                    # GPS object creation
+gps = GPS_SIMPLE(uart)                     # GPS object creation
 #########################################################################     
-
-        
 def get_lat_lon():
-    lat = lon = None # Opretter variabler med None som værdi
-    if gps.receive_nmea_data():
-    # hvis der er kommet end bruggbar værdi på alle der skal anvendes
+    lat = lon = None                       # create lat and lon variable with None as default value
+    if gps.receive_nmea_data():            # check if data is recieved
+                                           # check if the data is valid
         if gps.get_latitude() != -999.0 and gps.get_longitude() != -999.0 and gps.get_validity() == "A":
-            # gemmer returværdier fra metodekald i variabler
-            lat = str(gps.get_latitude())
-            lon = str(gps.get_longitude())
-            # returnerer data med adafruit gps format
-            return lat, lon
-        else: # hvis ikke både hastighed, latitude og longtitude er korrekte 
+            lat = str(gps.get_latitude())  # store latitude in lat variable
+            lon = str(gps.get_longitude()) # stor longitude in lon variable
+            return lat, lon                # multiple return values, needs unpacking or it will be tuple format
+        else:                              # if latitude and longitude are invalid
             print(f"GPS data to server not valid:\nlatitude: {lat}\nlongtitude: {lon}")
             return False
     else:
         return False
-# See examples for more authentication options
+                                           # Make client object to connect to thingsboard
 client = TBDeviceMqttClient(secrets.SERVER_IP_ADDRESS, access_token = secrets.ACCESS_TOKEN)
-
-# Connecting to ThingsBoard
-client.connect()
+client.connect()                           # Connecting to ThingsBoard
 print("connected to thingsboard, starting to send and receive data")
 while True:
     try:
         print(f"free memory: {gc.mem_free()}")
-        # monitor and free memory
-        if gc.mem_free() < 2000:
+        
+        if gc.mem_free() < 2000:          # monitor and free memory left
             print("Garbage collected!")
             gc.collect()
         
         lat_lon = get_lat_lon()
         print(lat_lon)
         if lat_lon:
-            # Sending telemetry        
+                                          # Sending telemetry        
             telemetry = {'latitude': lat_lon[0], 'longitude': lat_lon[1]}
             client.send_telemetry(telemetry)
         
-        # Checking for incoming subscriptions or RPC call requests (non-blocking)
+                                          # Checking for incoming subscriptions or RPC call requests (non-blocking)
         client.check_msg()
         print(".",end="")
         sleep(1)
     except KeyboardInterrupt:
         print("Disconnected!")
-        # Disconnecting from ThingsBoard
-        client.disconnect()
+        client.disconnect()               # Disconnecting from ThingsBoard
         reset()
+        
+        
+
